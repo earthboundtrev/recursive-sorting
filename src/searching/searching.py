2b80a55822c2a15079d84b205bd0aa38b7c7aea9
Trevor Thompson <Thompson_Trevor@bah.com>
2020-07-12 20:21:44 -0400

    Successfully created recursive implementation of binary search algorithm

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,28 @@
+import math
+# import ipdb
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    mid = math.floor(((end+start)/2))
+
+    if start == end+1:
+        return -1
+
+    # ipdb.set_trace()
+    if mid == end-1 or start+1 == mid:
+        return mid
+    else:
+        if(target < arr[mid]):
+            end = mid
+        elif(target == arr[mid]):
+            return mid
+        else:
+            start = mid
+        return binary_search(arr, target, start, end)
+
+    return -1
+
+# arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+# binary_search(arr1, 0, 0, len(arr1)-1)
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -10,5 +32,5 @@ def binary_search(arr, target, start, end):
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
-    # Your code here
+    pass
 
